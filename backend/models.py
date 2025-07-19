77918e8550278ec2e44ed6b7bbe9f0eed96b354c
HENL3Y <rdhenley93@gmail.com>
2025-07-19 16:38:25 +0100

    Updated review method in model.py

--- backend/models.py
+++ backend/models.py
@@ -1,21 +1,28 @@
 from . import db
 
 class Book(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
+    id = db.Column(db.Integer, primary_key=True) #Sets primary key.
+    title = db.Column(db.String(100), nullable=False) #nullable will prevent empty input.
     author = db.Column(db.String(100), nullable=False)
     reviews = db.relationship('Review', backref='user', lazy=True)
-
-    #def __repr__(self):
-        #return f"<Book {self.title}>"
 
 class User(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     username = db.Column(db.String(100), unique=True,nullable=False)
     reviews = db.relationship('Review', backref='user', lazy=True)
 
+class Review(db.model):
+    id = db.Column(db.Integer, primary_key=True)
+    rating = db.Column(db.Ingteger, nullable=True)
+    comment = db.Column(db.Text, nullable=True)
+    timestamp = db.Column(db.DateTime, nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False) #Creates foreign key from other methods.
+    book_id = db.Column(db.Integer, db.ForeignKey('bood.id'), nullable=False)
 
-# TODO: Create review table class. - https://www.notion.so/good-adventure-234614706360801c9af4ed7173b1d29c
+
+
+
+
 
 #Use this to set foreign key within table.
 #user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
